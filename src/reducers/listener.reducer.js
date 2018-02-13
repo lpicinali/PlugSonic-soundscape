785a0003289d8aff6e99d0bc3c1f9d76e6d4865a
Marco Comunità <m.comunita@imperial.ac.uk>
2018-02-13 11:37:16 +0000

    Move listener with mouse

---
+++ src/reducers/listener.reducer.js
@@ -0,0 +1,47 @@
+/* ------------------- NOTES -------------------- *//*
+
+*//* ---------------------------------------------- */
+
+import { ActionType } from 'src/constants.js'
+
+const initialState = {
+  listener: {
+    position: { azimuth: 0, distance: 0 },
+    isPerformanceModeEnabled: false,
+    headRadius: 0.0875,
+    // isDirectionalityEnabled: true,
+    // directionalityValue: 0,
+  }
+}
+
+export default function(state = initialState, { type, payload }) {
+  if (type === ActionType.SET_LISTENER_POSITION) {
+    const newListener = Object.assign({},state.listener);
+    Object.assign(newListener.position, payload.position);
+    // console.log("Action: SET TARGET POSITION");
+    // console.log(`Payload: ${payload}`);
+    // console.log("NEW STATE");
+    // console.log({ ...state, targets: newTargets });
+    return { ...state, listener: newListener };
+  }
+  if (type === ActionType.SET_HEAD_RADIUS) {
+    const newListener = Object.assign({},state.listener);
+    Object.assign(newListener.headRadius, payload.radius);
+    return { ...state, listener: newListener }
+  }
+  if (type === ActionType.SET_PERFORMANCE_MODE_ENABLED) {
+    const newListener = Object.assign({},state.listener);
+    Object.assign(newListener.isPerformanceModeEnabled, payload.isEnabled);
+    return { ...state, listener: newListener }
+  }
+
+  return state
+
+
+  // if (type === ActionType.SET_DIRECTIONALITY_ENABLED) {
+  //   return { ...state, isDirectionalityEnabled: payload.isEnabled }
+  // }
+  // if (type === ActionType.SET_DIRECTIONALITY_VALUE) {
+  //   return { ...state, directionalityValue: payload.value }
+  // }
+}
