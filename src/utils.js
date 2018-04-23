d239f02d5e1a045ac5dee16ea18811793d47100f
Marco Comunità <m.comunita@imperial.ac.uk>
2018-04-23 13:56:42 +0100

    Solves conflict between master and interactionArea

--- src/utils.js
+++ src/utils.js
@@ -12,6 +12,22 @@ export function radiusToCircumference(radius) {
   return radius * 2 * Math.PI
 }
 
+export function getDistanceBetweenSphericalPoints(a, b) {
+  const ax = Math.cos(a.azimuth) * a.distance
+  const az = Math.sin(a.azimuth) * a.distance
+  const bx = Math.cos(b.azimuth) * b.distance
+  const bz = Math.sin(b.azimuth) * b.distance
+
+  return Math.sqrt((bx - ax) ** 2 + (bz - az) ** 2)
+}
+
+export function fetchAudioBuffer(url) {
+  console.log('fetchAudioBuffer')
+  return got(url, { encoding: null })
+    .then(response => bufferToArrayBuffer(response.body))
+    .then(arrayBuffer => decode(arrayBuffer, context))
+}
+
 export function findTypeOfArray(array) {
   const protType = Object.prototype.toString.call(array)
   switch (protType) {
@@ -61,21 +77,6 @@ export function arrayToTypedArray(type, array) {
     default:
       return Uint8Array.from(array)
   }
-
-export function getDistanceBetweenSphericalPoints(a, b) {
-  const ax = Math.cos(a.azimuth) * a.distance
-  const az = Math.sin(a.azimuth) * a.distance
-  const bx = Math.cos(b.azimuth) * b.distance
-  const bz = Math.sin(b.azimuth) * b.distance
-
-  return Math.sqrt((bx - ax) ** 2 + (bz - az) ** 2)
-}
-
-export function fetchAudioBuffer(url) {
-  console.log('fetchAudioBuffer')
-  return got(url, { encoding: null })
-    .then(response => bufferToArrayBuffer(response.body))
-    .then(arrayBuffer => decode(arrayBuffer, context))
 }
 
 export function fetchAudioBufferRaw(rawObject) {
