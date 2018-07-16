d75b9aea3eda42d1270f8fe22428f36ea1fbabe6
Marco Comunità <m.comunita@imperial.ac.uk>
2018-07-16 10:27:13 +0100

    Allows CORS

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require('express')
+const cors = require('cors')
 const logger = require('morgan')
 
 const app = express()
@@ -7,10 +8,15 @@ const port = process.env.PORT || 8261
 
 app.use(logger('common'))
 app.use(express.static('public'))
+app.use(cors())
 // app.set('trust proxy', true)
 
 app.use((req, res) => {
   res.sendFile(path.resolve(__dirname, 'public/index.html'))
+})
+
+app.get('/products/:id', (req, res) => {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
 })
 
 app.listen(port, error => {
