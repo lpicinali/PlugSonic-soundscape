04f6e9c6ddb6f662171938ba95beb52450a75827
Alexander Wallin <office@alexanderwallin.com>
2019-02-06 15:06:24 +0100

    Makes soundscape map expand within elastic viewport

--- src/containers/Soundscape.js
+++ src/containers/Soundscape.js
@@ -5,14 +5,18 @@
 import React, {Component} from 'react'
 import { connect } from "react-redux"
 import PropTypes from 'prop-types'
+import Draggable from 'react-draggable'
 import styled from 'styled-components'
-
-import * as colors from 'src/styles/colors'
+import { clamp, pick } from 'lodash'
 
 import TabsContainer from 'src/containers/TabsContainer'
+import * as colors from 'src/styles/colors'
+
 // ===================================================================== //
 const drawerWidth = 288
 const navHeight = 48
+
+const MAP_SCALE = 10
 // ===================================================================== //
 
 const Container = styled.div`
@@ -30,24 +34,25 @@ const SoundscapeContainer = styled.div`
   height: 100%;
   margin-left: auto;
   margin-right: auto;
-  background: ${colors.YELLOW};
+  background: ${colors.WHITE};
 `
 
 const SoundscapeViewport = styled.div`
   position: relative;
-  width: 400px;
-  height: 400px;
-  border: 1px solid black;
+  overflow: hidden;
+  border: 1px solid ${colors.BLACK};
+`
+
+const SoundscapeDraggableBounds = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate3d(-50%, -50%, 0);
 `
 
 const SoundscapeRoom = styled.div`
   position: absolute;
-  top: 50%;
-  left: 50%;
-  width: ${props => props.roomWidth * 10}px;
-  height: ${props => props.roomDepth * 10}px;
-  transform: translate3d(-50%, -50%, 0);
-  background: yellow;
+  background-image: linear-gradient(to bottom right, ${colors.LIGHTBLUE}, ${colors.ORANGE});
 `
 
 const Drawer = styled.div`
@@ -62,31 +67,96 @@ const Drawer = styled.div`
 /* SOUNDSCAPE */
 /* ========================================================================== */
 class Soundscape extends Component {
+  state = {
+    containerBounds: {
+      width: 0,
+      height: 0,
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0,
+      x: 0,
+      y: 0,
+    },
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize)
+  }
+
+  $container = null
+
+  handleResize = () => {
+    if (this.$container !== null) {
+      this.storeContainerRef(this.$container)
+    }
+  }
+
+  storeContainerRef = ($container) => {
+    this.$container = $container
+
+    const bounds = $container.getBoundingClientRect()
+    this.setState({
+      containerBounds: {
+        width: bounds.width,
+        height: bounds.height,
+        top: bounds.top,
+        right: bounds.right,
+        bottom: bounds.bottom,
+        left: bounds.left,
+        x: bounds.x,
+        y: bounds.y,
+      }
+    })
+  }
+
   render() {
     const { width, height, showSettingsDrawer, roomWidth, roomDepth } = this.props
+    const { containerBounds } = this.state
 
-    // const roomRatio = roomWidth/roomDepth
-    // const newDrawerWidth = showSettingsDrawer ? drawerWidth : 0
-    // const containerWidth = width - newDrawerWidth
-    // const containerRatio = containerWidth/height
+    const mapWidth = roomWidth * MAP_SCALE
+    const mapHeight = roomDepth * MAP_SCALE
 
-    // let newWidth = 0;
-    // let newHeight = 0;
-    // if (roomRatio >= containerRatio) {
-    //   newWidth = containerWidth
-    //   newHeight = (containerWidth * 1/roomRatio) - navHeight
-    // } else {
-    //   newWidth = height * roomRatio
-    //   newHeight = height - navHeight
-    // }
+    const viewportWidth = Math.max(0, Math.min(containerBounds.width - 80, mapWidth))
+    const viewportHeight = Math.max(0, Math.min(containerBounds.height - 80, mapHeight))
+
+    const diffWidth = viewportWidth - mapWidth
+    const diffHeight = viewportHeight - mapHeight
+    const draggableAreaBleedWidth = Math.max(0, -diffWidth) * 2
+    const draggableAreaBleedHeight = Math.max(0, -diffHeight) * 2
 
     return (
       <Container>
-        <SoundscapeContainer>
-          <SoundscapeViewport>
-            <SoundscapeRoom roomWidth={roomWidth} roomDepth={roomDepth}>
-              Soundscape
-            </SoundscapeRoom>
+        <SoundscapeContainer ref={this.storeContainerRef}>
+          <SoundscapeViewport
+            style={{
+              width: viewportWidth,
+              height: viewportHeight,
+            }}
+          >
+            <SoundscapeDraggableBounds
+              style={{
+                width: `calc(100% + ${draggableAreaBleedWidth}px)`,
+                height: `calc(100% + ${draggableAreaBleedHeight}px)`,
+              }}
+            >
+              <Draggable
+                bounds="parent"
+              >
+                <SoundscapeRoom
+                  style={{
+                    width: mapWidth,
+                    height: mapHeight,
+                  }}
+                >
+                  Soundscape
+                </SoundscapeRoom>
+              </Draggable>
+            </SoundscapeDraggableBounds>
           </SoundscapeViewport>
         </SoundscapeContainer>
 
