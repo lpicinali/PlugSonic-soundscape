1ee8063c5451314622dde2a85137589244585047
Marco Comunità <m.comunita@imperial.ac.uk>
2019-06-05 17:52:22 +0100

    Implements exhibition using reducer

--- src/pluggy.js
+++ src/pluggy.js
@@ -81,13 +81,13 @@ const url = new URL((window.location !== window.parent.location)
             ? document.referrer
             : document.location.href)
 const hostname = url.hostname
-console.log('HOSTNAME')
-console.log(hostname)
+// console.log('HOSTNAME')
+// console.log(hostname)
 
 export let API
 // eslint-disable-next-line
-// export const sessionToken = Pluggy.getToken()
-export const sessionToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1YzQxYmJlZTYyN2E0ZWQ5OGZlMzRjMmEiLCJyb2xlcyI6WyJNZW1iZXIiLCJEZXZlbG9wZXIiXSwiYmVoYWxmT2ZVc2VySWQiOiI1YzQxYmJlZTYyN2E0ZWQ5OGZlMzRjMmEiLCJ1c2VybmFtZSI6Ik1hcmNvIENvbXVuaXRhIiwidGVhbVJvbGUiOiIiLCJraW5kIjoiVXNlclBlcnNvbiIsImlhdCI6MTU1OTcyODEwNiwiZXhwIjoxNTU5ODE0NTA2fQ.Dnd7q7gJ0LKbAF9lY2XvylVQiTKOxOZN_2yeEN_wr1U"
+export const sessionToken = Pluggy.getToken()
+// export const sessionToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1YzQxYmJlZTYyN2E0ZWQ5OGZlMzRjMmEiLCJyb2xlcyI6WyJNZW1iZXIiLCJEZXZlbG9wZXIiXSwiYmVoYWxmT2ZVc2VySWQiOiI1YzQxYmJlZTYyN2E0ZWQ5OGZlMzRjMmEiLCJ1c2VybmFtZSI6Ik1hcmNvIENvbXVuaXRhIiwidGVhbVJvbGUiOiIiLCJraW5kIjoiVXNlclBlcnNvbiIsImlhdCI6MTU1OTcyODEwNiwiZXhwIjoxNTU5ODE0NTA2fQ.Dnd7q7gJ0LKbAF9lY2XvylVQiTKOxOZN_2yeEN_wr1U"
 
 if (hostname === "develop.pluggy.eu") {
   API = "https://develop.pluggy.eu/api/v1"
@@ -111,29 +111,27 @@ export const exhibition = {
   // isPublished: false,
 }
 
-export let exhibitionMetadata = []
-
 if (hostname === "develop.pluggy.eu" || hostname === "beta.pluggy.eu") {
   const exhibitionQuery = window.location.search.substring(1)
-  console.log('EXHIBITION QUERY')
-  console.log(exhibitionQuery)
+  // console.log('EXHIBITION QUERY')
+  // console.log(exhibitionQuery)
   exhibition.id = getQueryVariable(exhibitionQuery,'exhibitionId')
   console.log('EXHIBITION ID')
   console.log(exhibition.id)
   httpGetSync(`${API}/exhibitions/${exhibition.id}`, getExhibitionCallback, getExhibitionErrorCallback, sessionToken)
 }
 
-if (hostname === "localhost") {
-  exhibition.id = "5cf63b3f96dc6b8929652f27"
-  console.log('EXHIBITION ID')
-  console.log(exhibition.id)
-  httpGetSync(`${API}/exhibitions/${exhibition.id}`, getExhibitionCallback, getExhibitionErrorCallback, sessionToken)
-}
+// if (hostname === "localhost") {
+//   exhibition.id = "5cf63b3f96dc6b8929652f27"
+//   console.log('EXHIBITION ID')
+//   console.log(exhibition.id)
+//   httpGetSync(`${API}/exhibitions/${exhibition.id}`, getExhibitionCallback, getExhibitionErrorCallback, sessionToken)
+// }
 
 function getExhibitionCallback(responseText) {
   const response = JSON.parse(responseText)
-  console.log('RETRIEVE EXHIBITION RESPONSE')
-  console.log(response)
+  // console.log('RETRIEVE EXHIBITION RESPONSE')
+  // console.log(response)
   if (response.success) {
     exhibition.description = response.data.description
     exhibition.isPublished = response.data.public
